dbed5e7c62fcc6801cda8c21b96b6e61f7ef8089
ShaanSoni123 <shaansoni21@gmail.com>
2025-10-16 22:29:22 +0530

    Configure GitHub Pages deployment with automatic workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: process.env.GITHUB_PAGES === 'true' ? '/munus/' : '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
